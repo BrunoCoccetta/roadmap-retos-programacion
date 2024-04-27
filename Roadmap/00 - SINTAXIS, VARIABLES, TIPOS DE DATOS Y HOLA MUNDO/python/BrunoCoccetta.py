0ade4176393b605c1ded2c5ece9d1d5f9ea9c461
BrunoCoccetta <brunococcetta@gmail.com>
2024-04-27 06:41:04 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BrunoCoccetta.py
@@ -0,0 +1,25 @@
+# https://www.python.org/
+
+#Esto es un comentario en una linea
+"""
+Este es un 
+comentario en 
+varias lineas
+"""
+
+'''
+Este tambien es un
+comentario en 
+varias lineas
+'''
+
+variable = "Esto es una vairable"
+
+CONSTANTE ="este lenguaje no posee constantes pero si lo escribo en mayusula podria tomarse como una"
+
+my_int=1
+my_float=1.3
+my_boolean=True
+My_string="esto es una cadena de texto"
+
+print("¡Hola, Python!")
